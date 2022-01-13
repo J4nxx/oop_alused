b28d5774f8bf208b7b2ac93ca8d75bb61e271541
Janre Liiv <janreliiv@gmail.com>
2022-01-13 11:32:56 +0200

    2.3 Kasutajad

---
+++ praks2/Kasutaja.py
@@ -0,0 +1,28 @@
+from praks2.Kasutajad import Kasutajad
+kasutaja1 = Kasutajad()
+kasutaja1.eesnimi = "Olaf"
+kasutaja1.perenimi = "Alakivi"
+kasutaja1.email = "olaf-olev.alakivi@voco.ee"
+kasutaja1.vanus = 16
+kasutaja1.kirjelda_kasutaja()
+kasutaja1.tervita_kasutaja()
+
+kasutaja2 = Kasutajad()
+kasutaja2.eesnimi = "Ax"
+kasutaja2.perenimi = "Hommuk"
+kasutaja2.email = "ax-sylvester.hommuk@voco.ee"
+kasutaja2.vanus = 17
+kasutaja2.kirjelda_kasutaja()
+kasutaja2.tervita_kasutaja()
+
+kasutaja3 = Kasutajad()
+kasutaja3.eesnimi = "Janre"
+kasutaja3.perenimi = "Liiv"
+kasutaja3.email = "janre.liiv@voco.ee"
+kasutaja3.vanus = 16
+kasutaja3.kirjelda_kasutaja()
+kasutaja3.tervita_kasutaja()
+
+
+
+
