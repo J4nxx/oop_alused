f9094851460419dd9df268acf88a77f946674d0f
Janre Liiv <janreliiv@gmail.com>
2022-01-20 09:09:05 +0200

    3.1 Restoraan

---
+++ praks2/Restoraan konstruktori abil.py
@@ -0,0 +1,6 @@
+from Restoran import Restoraan
+restoraan = Restoraan("Olafi Restoraan", "Lasanje")
+restoraan.kirjelda_restoraan()
+restoraan.ava_restoraan()
+
+
--- praks2/Restoran.py
+++ praks2/Restoran.py
@@ -1,7 +1,7 @@
 class Restoraan:
-    restoraani_nimi = ''
-    soogi_tyyp = ''
-
+    def __init__(self, restoraani_nimi, soogi_tyyp):
+        self.restoraani_nimi = restoraani_nimi
+        self.soogi_tyyp = soogi_tyyp
     def kirjelda_restoraan(self):
         kirj_tekst = self.restoraani_nimi, 'pakub', self.soogi_tyyp
         print(str(kirj_tekst))
