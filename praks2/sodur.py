3bb5634448f2f2a9dd13b435d70b342823c4861c
Janre Liiv <janreliiv@gmail.com>
2022-01-27 11:42:23 +0200

    Sõdurid2

--- praks2/sodur.py
+++ praks2/sodur.py
@@ -1,2 +1,6 @@
-class Sodur():
-    tervis = 100
+from praks2.sodurid2 import Inimene
+
+class Sodur(Inimene):
+    def __init__(self,armee_nr):
+        super().__init__()
+        self.armee_nr = armee_nr
